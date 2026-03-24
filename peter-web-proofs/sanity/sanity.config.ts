6f2d95aa04fa59c0d86e7dc3f0181ed8ee6c0e5a
AdamKatrenic <adamkatrenic@gmail.com>
2026-03-24 23:32:18 +0100

    Initial commit with sanity working

---
+++ peter-web-proofs/sanity/sanity.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { schemaTypes } from './schemaTypes/index'
+
+export default defineConfig({
+  name: 'default',
+  title: 'peter-web-proofs',
+
+  projectId: 'io727lbc',
+  dataset: 'production',
+
+  plugins: [structureTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
